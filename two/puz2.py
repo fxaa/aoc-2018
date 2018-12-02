f05c4b032d459c600006f5df55ed1a20b8657cb7
David Lewis <daveocado@gmail.com>
2018-12-02 15:15:45 -0600

    system exits are bad

--- two/puz2.py
+++ two/puz2.py
@@ -39,11 +39,15 @@ for i in range(len(lines)):
     for letter in lines[i]:
         totals[i] += scores[letter]
 
+found = False
 for i in range(len(lines)):
+    if found:
+        break
     for j in range(len(lines)):
         #don't even consider the pair if the score difference is too big to be crossed by one character
         if abs(totals[i] - totals[j]) < 26:
             diff = one_char_diff(lines[i], lines[j])
             if diff[0]:
                 print(lines[i][0:diff[1]] + lines[i][diff[1]+1:])
-                quit()
+                found = True
+                break
