881c0893b3e3e6a5f6d2dcff14f48d4caba72099
David Lewis <daveocado@gmail.com>
2018-12-07 02:39:44 -0600

    getting around to day 4

---
+++ four/puz1.py
@@ -0,0 +1,53 @@
+from datetime import *
+
+file = open('input.txt', 'r')
+lines = []
+line = file.readline()[:-1]
+while len(line) > 1:
+    lines.append(line)
+    line = file.readline()[:-1]
+
+
+for i in range(len(lines)):
+    lines[i] = [datetime.strptime(lines[i][1:17], "%Y-%m-%d %H:%M")] + lines[i][19:].split(' ')[0:2]
+
+lines.sort(key=lambda x: x[0])
+sleep_times = {}
+
+for i in range(len(lines)):
+    if lines[i][1] == "Guard":
+        current = lines[i][1] + " " + lines[i][2]
+    
+    elif lines[i][1] == "falls":
+        if current not in sleep_times:
+            sleep_times[current] = (timedelta(0), lines[i][0], [0 for i in range(60)])
+        else:
+            sleep_times[current] = (sleep_times[current][0], lines[i][0], sleep_times[current][2])
+    
+    elif lines[i][1] == "wakes":
+        time_now = lines[i][0]
+        old_time = sleep_times[current][1]
+        new_minutes = sleep_times[current][2]
+        for j in range(time_now.minute - old_time.minute):
+            new_minutes[j+old_time.minute] += 1
+        sleep_times[current] = (sleep_times[current][0] + (time_now - old_time), None, new_minutes)
+    
+longest = [timedelta(0)]
+for guard in sleep_times:
+    if sleep_times[guard][0] > longest[0]:
+        longest = sleep_times[guard]
+        worst = guard
+
+worst = int(worst.split(' ')[1][1:])
+minute_counts = longest[2]
+minute = -1
+idx = 0
+for i in range(len(minute_counts)):
+
+    if minute_counts[i] > minute:
+        print(i)
+        minute = minute_counts[i]
+        idx = i
+
+print(worst*idx)
+
---
+++ four/puz2.py
@@ -0,0 +1,51 @@
+from datetime import *
+
+file = open('input.txt', 'r')
+lines = []
+line = file.readline()[:-1]
+while len(line) > 1:
+    lines.append(line)
+    line = file.readline()[:-1]
+
+
+for i in range(len(lines)):
+    lines[i] = [datetime.strptime(lines[i][1:17], "%Y-%m-%d %H:%M")] + lines[i][19:].split(' ')[0:2]
+
+lines.sort(key=lambda x: x[0])
+sleep_times = {}
+minute_sleeps = [{} for i in range(60)]
+
+for i in range(len(lines)):
+    if lines[i][1] == "Guard":
+        current = lines[i][1] + " " + lines[i][2]
+    
+    elif lines[i][1] == "falls":
+        if current not in sleep_times:
+            sleep_times[current] = (timedelta(0), lines[i][0])
+        else:
+            sleep_times[current] = (sleep_times[current][0], lines[i][0])
+    
+    elif lines[i][1] == "wakes":
+        time_now = lines[i][0]
+        old_time = sleep_times[current][1]
+        for j in range(time_now.minute - old_time.minute):
+            if current in minute_sleeps[j+old_time.minute]:
+                minute_sleeps[j+old_time.minute][current] += 1
+            else: 
+                minute_sleeps[j+old_time.minute][current] = 1
+        sleep_times[current] = (sleep_times[current][0] + (time_now - old_time), None)
+    
+sleeper = None
+big_minute = 0
+big_minute_amount = 0
+for i in range(len(minute_sleeps)):
+    for guard in minute_sleeps[i]:
+        if minute_sleeps[i][guard] > big_minute_amount:
+            sleeper = guard
+            big_minute = i
+            big_minute_amount = minute_sleeps[i][guard]
+
+print(sleeper, big_minute, minute_sleeps[big_minute][sleeper])
+sleeper = int(sleeper.split(' ')[1][1:])
+
+print(sleeper*big_minute)
